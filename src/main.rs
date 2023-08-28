00a5585771dba81cf16baf5146b88f05e1cff570
OL <oiva.laitinen2@gmail.com>
2023-08-28 20:57:14 +0300

    Add basic logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+fn window_config() -> Conf {
+    Conf {
+        window_title: "Chess".to_string(),
+        window_width: 800,
+        window_height: 800,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_config)]
+async fn main() {
+    loop {
+        clear_background(WHITE);
+        if is_key_down(KeyCode::Q) {
+            break;
+        }
+        // Draw chess board
+        draw_board();
+        // Draw pieces from coordinates
+        next_frame().await
+    }
+}
 }
