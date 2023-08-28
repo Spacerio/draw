79741a217cb5719500a6d7c3444898a200909b18
OL <oiva.laitinen2@gmail.com>
2023-08-28 22:00:20 +0300

    Working prototype

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,24 @@ fn window_config() -> Conf {
 
 #[macroquad::main(window_config)]
 async fn main() {
+    let list = create_list();
+    let mut n = 0;
     loop {
-        clear_background(WHITE);
+        clear_background(BEIGE);
         if is_key_down(KeyCode::Q) {
             break;
         }
-        // Draw chess board
         draw_board();
-        // Draw pieces from coordinates
+        if is_key_pressed(KeyCode::Space) {
+            n += 1;
+        }
+        if is_key_pressed(KeyCode::P) {
+            n -= 1;
+        }
+
+        // draw_piece([1, 1, 2, 3]);
+        draw_piece(list.get(n).unwrap().to_owned());
+
         next_frame().await
     }
 }
@@ -30,13 +40,43 @@ fn draw_board() {
         for j in 0..8 {
             let x = i as f32 * w;
             let y = j as f32 * h;
-            if i % 2 == 0 {
-                if j % 2 == 0 {
-                    draw_rectangle(x, y, w, h, BLACK)
-                }
-            } else if j % 2 == 1 {
-                draw_rectangle(x, y, w, h, BLACK)
+            match (i % 2, j % 2) {
+                (1, 1) | (0, 0) => draw_rectangle(x, y, w, h, BROWN),
+                _ => ()
             }
         }
     }
+}
+
+fn draw_piece(l: [i32;4]) {
+    let r = 25.0;
+    let x = l[0] as f32 * (screen_width() / 8.0) - r * 2.0;
+    let y = l[1] as f32 * (screen_height() / 8.0) - r * 2.0;
+    draw_circle(x, y, r, WHITE);
+    let x = l[2] as f32 * (screen_width() / 8.0) - r * 2.0;
+    let y = l[3] as f32 * (screen_height() / 8.0) - r * 2.0;
+    draw_circle(x, y, r, BLACK);
+    println!("{:?}", l);
+}
+
+fn create_list() -> Vec<[i32;4]> {
+    let mut l: Vec<[i32;4]> = Vec::new();
+    let n = 8;
+    for wx in 1..=n {
+        for wy in 1..=n {
+            for bx in 1..=n {
+                for by in 1..=n {
+                    if wx != bx && wy != by {
+                        if (bx as i32 - by as i32).abs() != (wy as i32 - wx as i32).abs() {
+                            if (bx as i32 + by as i32).abs() != (wy as i32 + wy as i32).abs() {
+                                l.push([wx, wy, bx, by]);
+                                println!("{}", l.len());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    l
 }
