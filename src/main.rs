42ec0e139ebcf2bf56e6d55c97c8fec4b2285f97
OL <oiva.laitinen2@gmail.com>
2023-08-28 20:58:06 +0300

    Basic board drawing

--- src/main.rs
+++ src/main.rs
@@ -22,4 +22,21 @@ async fn main() {
         next_frame().await
     }
 }
+
+fn draw_board() {
+    let w = screen_width() / 8.0;
+    let h = screen_height() / 8.0;
+    for i in 0..8 {
+        for j in 0..8 {
+            let x = i as f32 * w;
+            let y = j as f32 * h;
+            if i % 2 == 0 {
+                if j % 2 == 0 {
+                    draw_rectangle(x, y, w, h, BLACK)
+                }
+            } else if j % 2 == 1 {
+                draw_rectangle(x, y, w, h, BLACK)
+            }
+        }
+    }
 }
